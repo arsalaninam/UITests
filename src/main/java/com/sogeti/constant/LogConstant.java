125c061df64038680eb224a634724551befcf456
arsalaninam <m.arsalaninam@gmail.com>
2020-07-26 20:19:57 +0200

    Add constants

---
+++ src/main/java/com/sogeti/constant/LogConstant.java
@@ -0,0 +1,8 @@
+package com.sogeti.constant;
+
+public interface LogConstant {
+
+    String TEST_SERVICE_AND_AUTOMATION_LINKS_ARE_SELECTED = "--Test to verify that the Services and Automation are selected--";
+    String TEST_CONTACT_US_FORM_DISPLAYS_MESSAGE = "--Test to verify Contact Us Form On Automation Link display valid message--";
+    String TEST_COUNTRY_SPECIFIC_SOGETI_LINKS_ARE_WORKING = "--Test to verify all the Country specific Sogeti links are working--";
+}
---
+++ src/main/java/com/sogeti/constant/TestConstant.java
@@ -0,0 +1,7 @@
+package com.sogeti.constant;
+
+public interface TestConstant {
+
+    long IMPLICIT_WAIT = 10;
+    long PAGE_LOAD_TIMEOUT = 15;
+}
